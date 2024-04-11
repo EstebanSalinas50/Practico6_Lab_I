e9e3e96362c9b8eb210589b8e722fde4ac343655
EstebanSalinas50 <estebankun31@gmail.com>
2024-04-11 11:40:52 -0300

    cambios

--- Practico6Lab/src/formularios/Menu.java
+++ Practico6Lab/src/formularios/Menu.java
@@ -51,6 +51,11 @@ public class Menu extends javax.swing.JFrame {
         jMenu1.setText("Administracion");
 
         jMenuItem1.setText("Productos");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem1);
 
         jMenuBar1.add(jMenu1);
@@ -113,6 +118,16 @@ public class Menu extends javax.swing.JFrame {
         escritorio.add(bpn);
         escritorio.moveToFront(bpn);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        escritorio.removeAll();
+        escritorio.repaint();
+        GestionProductos gestion= new GestionProductos();
+        gestion.setVisible(true);
+        escritorio.add(gestion);
+        escritorio.moveToFront(gestion);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
